2c74b235bf873f8e1dee342c3b49fae2fa6ced93
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-13 14:26:37 +0200

    update margin/padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.tsx
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-amber-400 p-4 text-center h-12">
+    <footer className="fixed bottom-0 w-full bg-gray-800 text-amber-400 p-4 text-center">
       <p>&copy; 2025 Happy Birthday to you</p>
     </footer>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ interface HeroProps {
 
 const Hero = ({ imageSrc, altText, children }: HeroProps) => {
   return (
-    <section className="relative w-full overflow-hidden">
+    <section className="relative w-full min-h-screen overflow-hidden">
       <Image
         src={imageSrc}
         alt={altText}
@@ -20,8 +20,8 @@ const Hero = ({ imageSrc, altText, children }: HeroProps) => {
       />
       <div className="absolute inset-0 bg-black/40" />
       
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4 text-center gap-4">
-        <h1 className="text-amber-400 text-5xl md:text-6xl marck font-bold drop-shadow-md">Grattis på födelsedagen!</h1>
+      <div className="relative z-10 flex flex-col items-center justify-center text-white px-4 text-center gap-4">
+        <h1 className="text-amber-400 text-5xl md:text-6xl marck font-bold drop-shadow-md py-3">Grattis på födelsedagen!</h1>
         {children}
       </div>
     </section>
